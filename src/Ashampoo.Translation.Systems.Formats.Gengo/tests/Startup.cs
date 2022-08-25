a284d3913778ea7d3a04dcb594447f6640625971
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-08-25 16:36:23 +0200

    load formats as plugin

--- src/Ashampoo.Translation.Systems.Formats.Gengo/tests/Startup.cs
+++ src/Ashampoo.Translation.Systems.Formats.Gengo/tests/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Ashampoo.Translation.Systems.Formats.Abstractions;
+﻿using System.IO;
+using Ashampoo.Translation.Systems.Formats.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Diagnostics;
 
 namespace Ashampoo.Translation.Systems.Formats.Gengo.Tests;
 
@@ -7,13 +9,12 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddFormatFactory().AddFormatProvider(builder =>
+        services.AddFormatFactory(configuration =>
         {
-            return builder.SetId("gengo")
-                .SetSupportedFileExtensions(new[] { ".xlsx", ".xls" })
-                .SetFormatType<GengoFormat>()
-                .SetFormatBuilder<GengoFormatBuilder>()
-                .Create();
+            var path = Path.GetDirectoryName(typeof(Startup).Assembly.Location);
+            Guard.IsNotNullOrWhiteSpace(path, nameof(path));
+            
+            configuration.PluginPaths.Add(path);
         });
     }
 }
