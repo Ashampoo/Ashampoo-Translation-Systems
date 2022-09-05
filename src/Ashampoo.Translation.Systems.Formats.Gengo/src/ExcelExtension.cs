8174222cfb181271f99a44795d92c1d21c2ae081
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-09-05 16:38:28 +0200

    fixed a bug, that would read translations without an id - minor

--- src/Ashampoo.Translation.Systems.Formats.Gengo/src/ExcelExtension.cs
+++ src/Ashampoo.Translation.Systems.Formats.Gengo/src/ExcelExtension.cs
@@ -10,8 +10,12 @@ public static class ExcelExtension
     /// <summary>
     /// Try to get the string value of the cell at index.
     /// </summary>
-    /// <param name="row"></param>
-    /// <param name="index"></param>
+    /// <param name="row">
+    /// The row to get the cell from.
+    /// </param>
+    /// <param name="index">
+    /// The zero-based index of the cell.
+    /// </param>
     /// <returns>The cell value as a string if it exists, otherwise null</returns>
     public static string? TryGetStringCellValue(this IRow row, int index)
     {
@@ -19,6 +23,33 @@ public static class ExcelExtension
         try
         {
             result = row.Cells[index].StringCellValue;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            // ignored
+        }
+
+        return result;
+    }
+    
+    /// <summary>
+    /// Try to get the cell at index.
+    /// </summary>
+    /// <param name="row">
+    /// The row to get the cell from.
+    /// </param>
+    /// <param name="index">
+    /// The zero-based index of the cell.
+    /// </param>
+    /// <returns>
+    /// The <see cref="ICell"/> if it exists, otherwise null.
+    /// </returns>
+    public static ICell? TryGetCell(this IRow row, int index)
+    {
+        ICell? result = default;
+        try
+        {
+            result = row.Cells[index];
         }
         catch (ArgumentOutOfRangeException)
         {
