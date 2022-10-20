7a0bb9ad1dde51127bc79868291d21eb6fcd7868
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-10-20 11:59:14 +0200

    tests: language parser tests

---
+++ src/Ashampoo.Translation.Systems.Formats.Abstractions/tests/LanguageParserTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Ashampoo.Translation.Systems.Formats.Abstractions.Tests;
+
+public class LanguageParserTests
+{
+    [Fact]
+    public void ParseLanguageCountryTest()
+    {
+        const string filePath = "Snap14-en-us.json";
+        var language = LanguageParser.TryParseLanguageId(filePath);
+        Assert.Equal("en-US", language);
+    }
+
+    [Fact]
+    public void ParseLanguageScriptTagCountryTest()
+    {
+        const string filePath = "Snap14-zh-Hant-TW.json";
+        var language = LanguageParser.TryParseLanguageId(filePath);
+        Assert.Equal("zh-Hant-TW", language);
+    }
+    
+
+    [Fact]
+    public void ValidLanguageCodeTest()
+    {
+        const string languageCodeChinese = "zh-Hant-TW";
+        const string languageCodeEnglish = "en-US";
+        var chineseIsValid = LanguageParser.IsValidLanguageCode(languageCodeChinese);
+        var englishIsValid = LanguageParser.IsValidLanguageCode(languageCodeEnglish);
+        Assert.True(chineseIsValid);
+        Assert.True(englishIsValid);
+    }
+}
