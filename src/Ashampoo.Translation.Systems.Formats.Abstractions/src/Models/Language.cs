d1ed6f168c5ad4342d79e4a9f762f7f337e70f19
Luca <lhi@ashampoo.com>
2024-02-21 09:50:17 +0100

    refactor: add comments

--- src/Ashampoo.Translation.Systems.Formats.Abstractions/src/Models/Language.cs
+++ src/Ashampoo.Translation.Systems.Formats.Abstractions/src/Models/Language.cs
@@ -3,18 +3,31 @@
 namespace Ashampoo.Translation.Systems.Formats.Abstractions.Models;
 
 /// <summary>
-/// 
+/// Represents a strongly typed identifier for a language.
 /// </summary>
 [StronglyTypedId(Template.String)]
 public readonly partial struct Language;
 
+/// <summary>
+/// Provides extension methods for the Language struct.
+/// </summary>
 public static class LanguageExtensions
 {
+    /// <summary>
+    /// Checks if the Language instance is null or its value is whitespace.
+    /// </summary>
+    /// <param name="language">The Language instance to check.</param>
+    /// <returns>True if the Language instance is null or its value is whitespace, otherwise false.</returns>
     public static bool IsNullOrWhitespace(this Language? language)
     {
         return string.IsNullOrWhiteSpace(language?.Value);
     }
     
+    /// <summary>
+    /// Checks if the value of the Language instance is whitespace.
+    /// </summary>
+    /// <param name="language">The Language instance to check.</param>
+    /// <returns>True if the value of the Language instance is whitespace, otherwise false.</returns>
     public static bool IsNullOrWhitespace(this Language language)
     {
         return string.IsNullOrWhiteSpace(language.Value);
