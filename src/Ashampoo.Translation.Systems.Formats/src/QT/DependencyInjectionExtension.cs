f7f958297b2184337c422ae2ec72bd05e791db29
Luca <lhi@ashampoo.com>
2024-03-15 08:16:39 +0100

    add: qtformat service registration, comments; changed: renamed SetComments -> GetComments, made doc.Save async

--- src/Ashampoo.Translation.Systems.Formats/src/QT/DependencyInjectionExtension.cs
+++ src/Ashampoo.Translation.Systems.Formats/src/QT/DependencyInjectionExtension.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Ashampoo.Translation.Systems.Formats.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ashampoo.Translation.Systems.Formats.QT;
 
+/// <summary>
+/// Static class that contains extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
 public static class DependencyInjectionExtension
 {
+    /// <summary>
+    /// Registers all necessary services for the QT format.
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to register the services with.
+    /// </param>
+    /// <returns>
+    /// The <see cref="IServiceCollection"/> for chaining.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if something went wrong during the registration.
+    /// </exception>
     public static IServiceCollection AddQtFormat(this IServiceCollection services)
     {
-        return services;
+        return services.AddSingleton<QtFormatProvider>()
+            .AddSingleton<IFormatProvider<IFormat>>(sp => sp.GetRequiredService<QtFormatProvider>())
+            .AddSingleton<IFormatProvider<QtFormat>>(sp => sp.GetRequiredService<QtFormatProvider>());
     }
 }
