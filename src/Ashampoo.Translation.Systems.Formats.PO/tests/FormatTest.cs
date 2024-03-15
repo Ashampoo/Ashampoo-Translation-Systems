a26868f27ad2b12b8262b4a0a49c71597ca8e67a
Luca <lhi@ashampoo.com>
2024-03-15 13:32:08 +0100

    add: comments to po translation file; refactor: Message.cs -> moved constants to external file; fix comments in po

--- src/Ashampoo.Translation.Systems.Formats.PO/tests/FormatTest.cs
+++ src/Ashampoo.Translation.Systems.Formats.PO/tests/FormatTest.cs
@@ -1,5 +1,5 @@
+using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Ashampoo.Translation.Systems.Formats.Abstractions;
 using Ashampoo.Translation.Systems.Formats.Abstractions.Models;
---
+++ src/Ashampoo.Translation.Systems.Formats/src/PO/POConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace Ashampoo.Translation.Systems.Formats.PO;
+
+/// <summary>
+/// 
+/// </summary>
+public static class POConstants
+{
+    /// <summary>
+    /// Identifier for the message context.
+    /// </summary>
+    public const string TypeMsgCtxt = "msgctxt ";
+
+    /// <summary>
+    /// Identifier for the message id.
+    /// </summary>
+    public const string TypeMsgId = "msgid ";
+
+    /// <summary>
+    /// Identifier for the message id plural.
+    /// </summary>
+    public const string TypeMsgIdPlural = "msgid_plural ";
+
+    /// <summary>
+    /// Identifier for the message string.
+    /// </summary>
+    public const string TypeMsgStr = "msgstr ";
+
+    public const string Divider = "/"; // TODO: move to interface?
+}
