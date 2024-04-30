f153cbaa757f5ba00e63772aa740f79c5a6459d2
Tjorven Kämpfer <62958434+tjorvenK@users.noreply.github.com>
2024-04-30 09:28:28 +0200

    fix(tsproj,ashlang): create empty comment lsit if no comment is set for translation

--- src/Ashampoo.Translation.Systems.Formats/src/AshLang/TargetTranslationString.cs
+++ src/Ashampoo.Translation.Systems.Formats/src/AshLang/TargetTranslationString.cs
@@ -24,7 +24,7 @@ public class TargetTranslationString : ITranslation
     {
         Language = language;
         _translation = translation;
-        Comments = [_translation.Comment];
+        Comments = string.IsNullOrWhiteSpace(translation.Comment) ? [] : [translation.Comment];
     }
 
     /// <inheritdoc />
