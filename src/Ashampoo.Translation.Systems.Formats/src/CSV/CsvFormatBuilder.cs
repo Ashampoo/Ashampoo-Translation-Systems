89bbfb36dbdb4f74d2c0c72f5965d3ccad800fc9
Luca <lhi@ashampoo.com>
2024-05-14 16:05:18 +0200

    feat: add csv format, add format builder and provider

---
+++ src/Ashampoo.Translation.Systems.Formats/src/CSV/CsvFormatBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Ashampoo.Translation.Systems.Formats.Abstractions;
+using Ashampoo.Translation.Systems.Formats.Abstractions.Models;
+using Ashampoo.Translation.Systems.Formats.Abstractions.Translation;
+using CommunityToolkit.Diagnostics;
+
+namespace Ashampoo.Translation.Systems.Formats.CSV;
+
+internal sealed class CsvFormatBuilder : IFormatBuilderWithTarget<CsvFormat>
+{
+    private Language _targetLanguage = Language.Empty;
+    private readonly Dictionary<string, string> _translations = new();
+    
+    public CsvFormat Build()
+    {
+        Guard.IsNotNullOrWhiteSpace(_targetLanguage.Value);
+
+        CsvFormat format = new()
+        {
+            Header =
+            {
+                TargetLanguage = _targetLanguage
+            }
+        };
+
+        foreach (var translation in _translations)
+        {
+            DefaultTranslationUnit unit = new(translation.Key);
+            DefaultTranslationString translationString = new(translation.Value, _targetLanguage, []);
+            unit.Translations.Add(translationString);
+            format.TranslationUnits.Add(unit);
+        }
+
+        return format;
+    }
+
+    public void SetHeaderInformation(IFormatHeader header)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddHeaderInformation(string key, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetTargetLanguage(Language language)
+    {
+        _targetLanguage = language;
+    }
+
+    public void Add(string id, string target)
+    {
+        _translations.Add(id, target);
+    }
+}
