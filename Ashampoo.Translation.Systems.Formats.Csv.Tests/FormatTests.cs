8bb9be0c8ec6ae9af8e124496460a4ed9d14cde0
Luca <lhi@ashampoo.com>
2024-06-11 11:58:23 +0200

    feat: add new csv format support (read&write), refactor(tests): removed fluentassertions from test projects except the base test project, chore(tests): updated packages

--- Ashampoo.Translation.Systems.Formats.Csv.Tests/FormatTests.cs
+++ Ashampoo.Translation.Systems.Formats.Csv.Tests/FormatTests.cs
@@ -1,17 +1,18 @@
-﻿using Ashampoo.Translation.Systems.Formats.Abstractions;
+using Ashampoo.Translation.Systems.Formats.Abstractions;
 using Ashampoo.Translation.Systems.Formats.Abstractions.Models;
 using Ashampoo.Translation.Systems.Formats.Abstractions.Translation;
+using Ashampoo.Translation.Systems.Formats.CSV;
 using Ashampoo.Translation.Systems.TestBase;
 using FluentAssertions;
 
-namespace Ashampoo.Translation.Systems.Formats.JavaProperties.Test;
+namespace Ashampoo.Translation.Systems.Formats.Csv.Tests;
 
-public class FormatTest : FormatTestBase<JavaPropertiesFormat>
+public class FormatTests : FormatTestBase<CsvFormat>
 {
     [Fact]
     public void NewFormat()
     {
-        IFormat format = CreateFormat();
+        var format = CreateFormat();
 
         format.Should().NotBeNull();
         format.TranslationUnits.Should().BeEmpty();
@@ -22,44 +23,43 @@ public class FormatTest : FormatTestBase<JavaPropertiesFormat>
     [Fact]
     public void ReadFromFile()
     {
-        IFormat format =
-            CreateAndReadFromFile("messages_de.properties",
-                new FormatReadOptions { TargetLanguage = new Language("de-DE") });
-        const string id = "aboutTheApp";
+        var format =
+            CreateAndReadFromFile("testfile_en.csv",
+                new FormatReadOptions
+                    { TargetLanguage = new Language("de-DE"), SourceLanguage = new Language("en-US") });
+        const string id = "HOME.HOME";
 
-        foreach (var unit in format.TranslationUnits)
-        {
-            unit.Translations.Should().ContainSingle();
-        }
-
-        format.TranslationUnits.Count.Should().Be(186);
+        format.TranslationUnits.Count.Should().Be(2);
 
         var foundById = format.TranslationUnits.GetTranslationUnit(id);
         foundById.Should().NotBeNull();
         var translationString = foundById.Translations.GetTranslation(new Language("de-DE"));
         translationString.Should().NotBeNull();
-        translationString.Value.Should().Be("Über Photos");
+        translationString.Value.Should().Be("Haus, Zuhause");
         translationString.Comments.Should().BeEmpty();
+        var sourceTranslation = foundById.Translations.GetTranslation(new Language("en-US"));
+        sourceTranslation.Should().NotBeNull();
+        sourceTranslation.Value.Should().Be("Home");
     }
 
     [Fact]
     public void WriteFormatLeavesStreamOpen()
     {
-        var format = CreateAndReadFromFile("messages_de.properties",
-            new FormatReadOptions { TargetLanguage = new Language("de-DE") });
-        
+        IFormat format = CreateAndReadFromFile("testfile_en.csv",
+            new FormatReadOptions { TargetLanguage = new Language("de-DE"), SourceLanguage = new Language("en-US") });
+
         var memoryStream = new MemoryStream();
         format.Write(memoryStream);
         memoryStream.CanRead.Should().BeTrue();
         memoryStream.CanWrite.Should().BeTrue();
     }
-    
+
     [Fact]
     public async Task WriteFormatLeavesStreamOpenAsync()
     {
-        var format = await CreateAndReadFromFileAsync("messages_de.properties",
-            new FormatReadOptions { TargetLanguage = new Language("de-DE") });
-        
+        var format = await CreateAndReadFromFileAsync("testfile_en.csv",
+            new FormatReadOptions { TargetLanguage = new Language("de-DE"), SourceLanguage = new Language("en-US") });
+
         var memoryStream = new MemoryStream();
         await format.WriteAsync(memoryStream);
         memoryStream.CanRead.Should().BeTrue();
