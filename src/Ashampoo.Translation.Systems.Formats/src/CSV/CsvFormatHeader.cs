1ff852e87953af0830df82cbd5c6a402be44ddeb
Luca <lhi@ashampoo.com>
2024-06-11 14:30:04 +0200

    add(formats): custom csvformatheader, add comments, detecting delimiter if not set

---
+++ src/Ashampoo.Translation.Systems.Formats/src/CSV/CsvFormatHeader.cs
@@ -0,0 +1,37 @@
+﻿using Ashampoo.Translation.Systems.Formats.Abstractions;
+using Ashampoo.Translation.Systems.Formats.Abstractions.Models;
+
+namespace Ashampoo.Translation.Systems.Formats.CSV;
+
+/// <summary>
+/// Header for the <see cref="CsvFormat"/>.
+/// </summary>
+public sealed class CsvFormatHeader : IFormatHeader
+{
+    /// <inheritdoc />
+    public Language TargetLanguage { get; set; }
+
+    /// <inheritdoc />
+    public Language? SourceLanguage { get; set; }
+
+    /// <summary>
+    /// The delimiter (separator) to use for the csv file.
+    /// </summary>
+    public char Delimiter { get; set; } = ';';
+
+    /// <inheritdoc />
+    public Dictionary<string, string> AdditionalHeaders { get; set; } = [];
+
+    /// <summary>
+    /// Writes the header information stored in this format header to the textwriter.
+    /// </summary>
+    /// <param name="writer">
+    /// The writer to write the header information to.
+    /// </param>
+    public async Task WriteHeader(TextWriter writer)
+    {
+        await writer.WriteLineAsync($"#Target Language: {TargetLanguage.Value}");
+        await writer.WriteLineAsync($"#Source Language: {SourceLanguage?.Value}");
+        await writer.WriteLineAsync($"#Delimiter: {Delimiter}");
+    }
+}
