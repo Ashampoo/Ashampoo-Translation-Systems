e206c3bd9ae4a6ecf259748c68864f3452ea492c
RealLHI <lhi@ashampoo.com>
2024-07-18 07:39:32 +0200

    tests(abstractions): add test cases for LanguageParser

---
+++ Ashampoo.Translation.Systems.Formats.Abstractions.Tests/LanguageParserTests.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+
+namespace Ashampoo.Translation.Systems.Formats.Abstractions.Tests;
+
+public class LanguageParserTests
+{
+    [Theory]
+    [InlineData("App.en-US.resx", "en-US")]
+    [InlineData("App.en.resx", "en")]
+    [InlineData("App_en.resx", "en")]
+    [InlineData("App-en.resx", "en")]
+    [InlineData("App_en-US.resx", "en-US")]
+    [InlineData("App-en-US.resx", "en-US")]
+    public void ShouldParseLanguageFromFileName(string fileName, string expectedLanguage)
+    {
+        LanguageParser.TryParseLanguageId(fileName).Should().NotBeNullOrWhiteSpace().And.Be(expectedLanguage);
+    }
+}
